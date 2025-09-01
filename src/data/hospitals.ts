e962de32c80ef4c5ce82aa9d75f7e376233b8f97
Harsh Pathak <hp238413@gmail.com>
2025-09-01 17:40:16 +0000

    make all delhi hospitals in near by  hospitalslike RML hospital, AIMS De

--- src/data/hospitals.ts
+++ src/data/hospitals.ts
@@ -1,9 +1,9 @@
 export const hospitals = [
-  { id: 1, name: "City General Hospital", address: "123 Health St, Downtown, Mumbai", type: "General", latitude: 19.0760, longitude: 72.8777 },
-  { id: 2, name: "Apollo Spectra Hospitals", address: "Sion, Mumbai", type: "Multi-speciality", latitude: 19.0445, longitude: 72.8635 },
-  { id: 3, name: "Lilavati Hospital & Research Centre", address: "Bandra West, Mumbai", type: "Multi-speciality", latitude: 19.0628, longitude: 72.8322 },
-  { id: 4, name: "Fortis Hospital", address: "Mulund West, Mumbai", type: "Multi-speciality", latitude: 19.1731, longitude: 72.9495 },
-  { id: 5, "name": "Apex Hospitals", "address": "Borivali West, Mumbai", "type": "Multi-speciality", "latitude": 19.2311, "longitude": 72.8532 },
-  { id: 6, name: "Surya Children's Hospital", address: "Santacruz West, Mumbai", type: "Pediatric", latitude: 19.0827, longitude: 72.8417 },
-  { id: 7, name: "Hinduja Healthcare Surgical", address: "Khar West, Mumbai", type: "Surgical", latitude: 19.0706, longitude: 72.8378 },
+  { id: 1, name: "All India Institute of Medical Sciences (AIIMS)", address: "Ansari Nagar, New Delhi, Delhi 110029", type: "Public Hospital", latitude: 28.5671, longitude: 77.2081 },
+  { id: 2, name: "Safdarjung Hospital", address: "Ansari Nagar East, New Delhi, Delhi 110029", type: "Multi-speciality", latitude: 28.5704, longitude: 77.2078 },
+  { id: 3, name: "Dr. Ram Manohar Lohia Hospital (RML)", address: "Baba Kharak Singh Marg, Connaught Place, New Delhi, Delhi 110001", type: "Multi-speciality", latitude: 28.6232, longitude: 77.2065 },
+  { id: 4, name: "Max Healthcare Saket", address: "Saket Institutional Area, Saket, New Delhi, Delhi 110017", type: "Private Hospital", latitude: 28.5283, longitude: 77.2155 },
+  { id: 5, name: "Indraprastha Apollo Hospitals", address: "Delhi Mathura Road, Sarita Vihar, New Delhi, Delhi 110076", type: "Multi-speciality", latitude: 28.5385, longitude: 77.2849 },
+  { id: 6, name: "Sir Ganga Ram Hospital", address: "Rajinder Nagar, New Delhi, Delhi 110060", type: "Multi-speciality", latitude: 28.6360, longitude: 77.1855 },
+  { id: 7, name: "Fortis Escorts Heart Institute", address: "Okhla road, New Friends Colony, New Delhi, Delhi 110025", type: "Cardiac", latitude: 28.5630, longitude: 77.2758 },
 ];
