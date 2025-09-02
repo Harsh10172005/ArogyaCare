24a4910f253242938c8debdb508421c87c153684
Harsh Pathak <hp238413@gmail.com>
2025-09-02 13:04:40 +0000

    make all free health care camp from delhi

--- src/data/camps.ts
+++ src/data/camps.ts
@@ -1,7 +1,7 @@
 export const camps = [
   { id: 1, title: "Free Dental Check-up Camp", location: "Community Hall, Park Street, Delhi", date: "2024-10-15", registrationLink: "#" },
-  { id: 2, title: "General Health & Eye Check-up", location: "Public School Ground, Sector 12, Noida", date: "2024-10-22", registrationLink: "#" },
-  { id: 3, title: "Diabetes & Blood Pressure Screening", location: "Senior Citizen Center, Koramangala, Bangalore", date: "2024-11-05", registrationLink: "#" },
-  { id: 4, title: "Women's Health Awareness Camp", location: "Mahila Mandal Hall, Jayanagar, Bangalore", date: "2024-11-12", registrationLink: "#" },
-  { id: 5, title: "Children's Vaccination Drive", location: "City Clinic, Anna Nagar, Chennai", date: "2024-11-20", registrationLink: "#" },
+  { id: 2, title: "Comprehensive Health Screening", location: "Community Center, Dwarka, New Delhi", date: "2024-10-25", registrationLink: "#" },
+  { id: 3, title: "Eye Care and Vision Testing", location: "Govt. School, Rohini, New Delhi", date: "2024-11-08", registrationLink: "#" },
+  { id: 4, title: "Women's Wellness Camp", location: "Mahila Shakti Kendra, Lajpat Nagar, Delhi", date: "2024-11-18", registrationLink: "#" },
+  { id: 5, title: "Child Health & Immunization Drive", location: "Primary Health Center, Vasant Kunj, New Delhi", date: "2024-11-25", registrationLink: "#" },
 ];
--- src/lib/firebase-credentials.ts
+++ src/lib/firebase-credentials.ts
@@ -9,13 +9,14 @@
 
 export const serviceAccount = {
   "type": "service_account",
-  "project_id": "YOUR_PROJECT_ID",
-  "private_key_id": "YOUR_PRIVATE_KEY_ID",
-  "private_key": "-----BEGIN PRIVATE KEY-----\\nYOUR_PRIVATE_KEY\\n-----END PRIVATE KEY-----\\n",
-  "client_email": "YOUR_CLIENT_EMAIL",
-  "client_id": "YOUR_CLIENT_ID",
+  "project_id": "arogyacare-yf3yk",
+  "private_key_id": "52937616a2770212625598df31bbd07c3fea7a3e",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCzmjCoaLgRAmoA\nEjNU76O1JFmv3JNahhL/nC0U3iHR2NoaNKB7ZWfoCMo0J0iOyfBz9VJLd473XIId\nFD+Pf+6t8Yqkm7tIj5Za/giozwE8My1josO5+WFB4pZ60/IV5b7WxSbBYiJpHWuD\nmaocV8Xd//cqeoZAPPjH8Mn5QB83FUMLiK/b5f0Q6MH1C6I/yi1pvarMDDjOuteM\nLah9cdVFvhfT5g9FQ/CSvyPxdSESnOO2NuoKgzn+iINwNXQraab1JgnJpbQTf8fB\nf6La7+CEtALl1G55g08++s85AoQZqTn9gJzON39j2CsMRuEqj0sHKp9aiQ7bzSZl\n4uZVpKjfAgMBAAECggEACecXBrobaAivioh2XxkFeD/kWA976nJZCtH0JGHpEHZ5\n9BbxwEwDkiYl7AE8fW8B/IrKulGBxv7JR8wtgv6FTP7z64aQqXwNSMAX/yLGyShz\nOLqpOxBWffj93hDgmLd9sb7WF7SO/Ce8nVl0KTLnvIlX+fYef908xF1w7XbNIc7I\n2giwX744jwH4E6VvHtYcjVjuy5zjxGzchb2HJulm5+li/Wo8afQRLFXg/NLfCkeH\n6FT0Umx0IqpS6LriVqOCImCmqRRTyiuoKX6ckZ3iDf3BCq7wvDf/bNLc+NhCJtbk\nwEPaeSP/TXWlPOi4sI1B5bgkX+ZIBbaVLkAaLGilLQKBgQD8KVozOdf2CVTyAqUk\n4XSnLC2ZiqTjJHqVgfa8BB/u8cY3CK+OSCCelyrooSViHjIDq8nW9WMhvNJEVq7B\nVc4fGLOxaIDz6otFED6RTF/l3Oxlk8UWiQHOIM8t9gK3QmTTU93pQXLKLAyUSKyQ\nrTDcH97hpLcF/5Kqoxw+M5RW5QKBgQC2VhT0HmG+0mfq5TjqzjfQB2cJq/o9u34l\nKcXKixAY47Fk1A12B5/biTsV0nbu3vtQ1h3SsVHuKoNNp5X/D/66+/PiKFONpbe8\nqy1wnYOjzsswnNasRQsch6yeV6JkBKP3EeCRZx26rRVlWHOyV+U4Y7urkiODLODo\nKotmY0EgcwKBgEQBtxT3tbniXGmEsxtWPsrTTF0Lmlq0POdTjMK6bg5YYHf5PLen\n1jxPUsA4rcdfk2eQDjiOJ7UyopbRtgHobVXPqK7hG14tVHBE6ErxiS+KPKtMhiwE\nuVrpWFbx8VuRaXXhOYkuIJJL/PyE+R1vyCyl38uFS+MvQVQJ95UC/i/NAoGAFgeE\ndJIPISzunTSgkc3kdF/QlRyQGVFDxl2rpfLzOO6ZiiMMlUwKhk2oM9S/xdBu8K9N\n3xzJV0sPnhw3RR+7mP/D0Xkpzx1Me6D61nZRZ0MezXrFfP7CfuntcxwHZKoi3UDt\nruOUde9ffGUZZVy1NwDlta7sV3benW42dWJchf0CgYEAsdpY6Jbyv2o7tQPiNAeh\nkNP05O87kQmEt2PV+n9LacbvOkmeqrUjhPITKOsrSs3lrUeBK+bMJjxTkTwLpnfG\nAhC8Y6fE2Rz5PEc52V0aAHtIc/qC8mm7FSlZd41iygVNgb1URF+5JufHSAR+7BMy\nNUb68hPExql66JUYt8odVHU=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@arogyacare-yf3yk.iam.gserviceaccount.com",
+  "client_id": "116867354368837081332",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "YOUR_CLIENT_X509_CERT_URL"
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40arogyacare-yf3yk.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
 };
