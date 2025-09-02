6c9af44b0d3fd800b65e1615a4190bc178a66945
Harsh Pathak <hp238413@gmail.com>
2025-09-02 13:15:33 +0000

    make heart more bigger for me please

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -25,7 +25,7 @@ export default function Hero() {
         <div className="relative flex justify-center items-center h-60">
             <div className="absolute inset-0 bg-primary/10 rounded-full blur-3xl"></div>
             <div className="relative w-full h-full flex flex-col items-center justify-center">
-                <Heart className="h-24 w-24 text-primary/80 fill-primary/20 animate-beat mb-4" />
+                <Heart className="h-32 w-32 text-primary/80 fill-primary/20 animate-beat mb-4" />
                 <svg
                     viewBox="0 0 400 150"
                     className="w-full h-auto"
