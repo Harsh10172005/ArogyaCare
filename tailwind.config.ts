2acacba949b14d4d58130219a1fb4cc18a45e96f
Harsh Pathak <hp238413@gmail.com>
2025-09-02 13:11:41 +0000

    make heart move bigger like before it is

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -88,7 +88,7 @@ export default {
         },
         'beat': {
           '0%, 100%': { transform: 'scale(1)' },
-          '50%': { transform: 'scale(1.1)' },
+          '50%': { transform: 'scale(1.25)' },
         },
         'pulse-line': {
             '0%': { strokeDasharray: '0, 1000' },
