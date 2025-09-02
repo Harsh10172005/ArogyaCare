ec994f642681ac6544bf8a8c1581712d21e12fc3
Harsh Pathak <hp238413@gmail.com>
2025-09-02 08:41:46 +0000

    and make padding from left to ArogyaCare text in footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,8 +5,8 @@ export default function Footer() {
   return (
     <footer className="bg-muted text-muted-foreground">
       <div className="container py-12 text-center">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
-          <div className="sm:col-span-2 md:col-span-1 md:text-left">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 pb-8">
+          <div className="sm:col-span-2 md:col-span-1 md:text-left md:pl-8">
             <Link href="/" className="mb-4 inline-block text-xl font-bold text-primary">
                 ArogyaCare
             </Link>
