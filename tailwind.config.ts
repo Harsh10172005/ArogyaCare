e15c228d7ba7b39f83ded82cd36f3ff4ceea88b5
Harsh Pathak <hp238413@gmail.com>
2025-09-04 08:47:15 +0000

    make heart beating animation of scale 1 to 2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -89,7 +89,7 @@ export default {
         },
         'beat': {
           '0%, 100%': { transform: 'scale(1)' },
-          '50%': { transform: 'scale(3)' },
+          '50%': { transform: 'scale(2)' },
         },
         'pulse-line-draw': {
           'from': { 'stroke-dashoffset': '1000' },
