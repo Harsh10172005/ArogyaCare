92d0ea4ce2ef4aef622eee4a51f8aeef520c8e6e
Harsh Pathak <hp238413@gmail.com>
2025-09-04 08:48:22 +0000

    increase beating time and try to sync with pulse

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -99,7 +99,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'beat': 'beat 1.5s ease-in-out infinite',
+        'beat': 'beat 3s ease-in-out infinite',
         'pulse-line-draw': 'pulse-line-draw 3s ease-in-out infinite',
       },
     },
